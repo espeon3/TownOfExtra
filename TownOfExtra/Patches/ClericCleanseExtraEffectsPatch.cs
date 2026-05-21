bbc561a2d0cba0d50ca543b03f933d56ed3ee9f6
Mehzxzz <243104136+Mehzxzz@users.noreply.github.com>
2026-05-21 20:15:28 +0100

    Added support for cleric's cleanse ability (Ty divani for his code from divani mods)

---
+++ TownOfExtra/Patches/ClericCleanseExtraEffectsPatch.cs
@@ -0,0 +1,16 @@
+﻿using HarmonyLib;
+using TownOfExtra.Utilities;
+using TownOfUs.Modifiers.Crewmate;
+
+namespace TownOfExtra.Patches;
+public static class ClericCleanseExtraEffectsPatch
+{
+    [HarmonyPatch(typeof(ClericCleanseModifier), "CleansePlayer")]
+    private static class CleansePlayerPatch
+    {
+        private static void Postfix(ClericCleanseModifier __instance)
+        {
+            NegativeEffects.CleanseAll(__instance.Player);
+        }
+    }
+}
---
+++ TownOfExtra/Utilities/NegativeEffects.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using MiraAPI.Modifiers;
+
+namespace TownOfExtra.Utilities;
+
+public static class NegativeEffects
+{
+    private static readonly List<Action<PlayerControl, ModifierComponent>> Removers =
+    [
+        // general
+        Remover<Modifiers.ErasedModifier>(),
+        Remover<Modifiers.PoisonedModifier>(),
+        Remover<Modifiers.SwitchedModifier>(),
+        // lucid dreams
+        Remover<Modifiers.LucidDreamingModifier>(),
+        Remover<Modifiers.WaitingOnLcdModifier>(),
+    ];
+
+    public static void CleanseAll(PlayerControl player)
+    {
+        if (player == null)
+        {
+            return;
+        }
+
+        var comp = player.GetModifierComponent();
+        if (comp == null)
+        {
+            return;
+        }
+
+        foreach (var remove in Removers)
+        {
+            remove(player, comp);
+        }
+    }
+
+    private static Action<PlayerControl, ModifierComponent> Remover<T>()
+        where T : BaseModifier
+    {
+        return (player, comp) =>
+        {
+            foreach (var modifier in player.GetModifiers<T>().ToArray())
+            {
+                comp.RemoveModifier(modifier);
+            }
+        };
+    }
+}
