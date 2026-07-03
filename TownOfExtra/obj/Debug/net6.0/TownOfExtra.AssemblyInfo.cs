9821bb6168e519e9826cb63a6c7de5038150d02b
Mehzxzz <243104136+Mehzxzz@users.noreply.github.com>
2026-07-03 11:45:03 +0100

    update modNews-en_US.json

--- TownOfExtra/obj/Debug/net6.0/TownOfExtra.AssemblyInfo.cs
+++ TownOfExtra/obj/Debug/net6.0/TownOfExtra.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyDescriptionAttribute("An addon to Town of Us: Mira including extra roles and features!")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+001a8cc02abee3a8bdcc584af1bdc9b1384bad9f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0ff132591d4f35c961a2e69df0ee2cf37c995ba7")]
 [assembly: System.Reflection.AssemblyProductAttribute("TownOfExtra")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TownOfExtra")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
