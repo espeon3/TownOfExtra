d734e0785ea1f8537f441fe87a5c15c3f3b77ecf
espeon3 <espeonicalic2@gmail.com>
2026-07-09 13:58:50 +0200

    Create EmbrittlementBrittleButton.cs

---
+++ TownOfExtra/Buttons/Modifiers/EmbrittlementBrittleButton.cs
@@ -0,0 +1,29 @@
+using MiraAPI.GameOptions;
+using MiraAPI.Keybinds;
+using MiraAPI.Modifiers;
+using MiraAPI.Utilities.Assets;
+using TownOfExtra.Modifiers.Excluded;
+using TownOfExtra.Options.Roles;
+using TownOfExtra.Roles.Neutral.Killing;
+using TownOfUs.Buttons;
+using TownOfUs.Options.Modifiers.Alliance;
+using TownOfUs.Utilities;
+using UnityEngine;
+
+namespace TownOfExtra.Buttons;
+
+public sealed class EmbrittlementBrittleButton : TownOfUsRoleButton<EmbrittlementModifier, PlayerControl>
+{
+    public override string Name => "Target";
+    public override BaseKeybind Keybind => Keybinds.SecondaryAction;
+    public override Color TextOutlineColor => Pallette.ImpostorRed;
+    public override float Cooldown => OptionGroupSingleton<ImpostorModifierOptions>.Instance.BrittleCooldown;
+    public override float EffectDuration => OptionGroupSingleton<ImpostorModifierOptions>.Instance.BrittleDuration;
+    public override LoadableAsset<Sprite> Sprite => TownOfExtraAssets.PhMisc;
+
+    protected override void OnClick()
+    {
+        if (Target == null) return;
+
+        Target.RpcAddModifier<EmbrittlementBrittleModifier>();
+    }
