798fdbfd1fc5ef1459e19b7ebcf8b9606310b818
Mehzxzz <243104136+Mehzxzz@users.noreply.github.com>
2026-07-10 08:35:11 +0100

    Made shifter unlovable

--- TownOfExtra/Roles/Neutral/Outlier/ShifterRole.cs
+++ TownOfExtra/Roles/Neutral/Outlier/ShifterRole.cs
@@ -4,6 +4,7 @@ using AmongUs.GameOptions;
 using Il2CppInterop.Runtime.Attributes;
 using MiraAPI.Roles;
 using TownOfUs.Extensions;
+using TownOfUs.Interfaces;
 using TownOfUs.Modules.Wiki;
 using TownOfUs.Roles;
 using TownOfUs.Roles.Neutral;
@@ -12,12 +13,13 @@ using UnityEngine;
 
 namespace TownOfExtra.Roles.Neutral.Outlier;
 
-public sealed class ShifterRole(IntPtr cppPtr) : NeutralRole(cppPtr), ITownOfUsRole, IWikiDiscoverable, IDoomable, IUnguessable
+public sealed class ShifterRole(IntPtr cppPtr) : NeutralRole(cppPtr), ITownOfUsRole, IWikiDiscoverable, IDoomable, IUnguessable, IUnlovable
 {
     public string RoleName => "Shifter";
     public string RoleDescription => "Shift roles with another player!";
     public string RoleLongDescription => RoleDescription;
     public bool IsGuessable => false;
+    public bool IsUnlovable => true;
     public RoleBehaviour AppearAs => RoleManager.Instance.GetRole((RoleTypes)RoleId.Get<ShifterRole>());
     public Color RoleColor => TownOfExtraColours.ShifterRoleColour;
     public ModdedRoleTeams Team => ModdedRoleTeams.Custom;
