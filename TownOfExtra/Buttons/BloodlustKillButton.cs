4e4eb79fd2ee917f139d70d1b426cad4d57afa6f
espeon3 <espeonicalic2@gmail.com>
2026-07-22 08:43:26 +0200

    Update BloodlustKillButton.cs

--- TownOfExtra/Buttons/BloodlustKillButton.cs
+++ TownOfExtra/Buttons/BloodlustKillButton.cs
@@ -1,4 +1,4 @@
-using MiraAPI.GameOptions;
+﻿using MiraAPI.GameOptions;
 using MiraAPI.Keybinds;
 using MiraAPI.Networking;
 using MiraAPI.Utilities.Assets;
@@ -7,18 +7,21 @@ using TownOfExtra.Roles.Impostor.Killing;
 using TownOfUs.Assets;
 using TownOfUs.Buttons;
 using TownOfUs.Networking;
+using TownOfUs.Options;
+using TownOfUs.Options.Maps;
 using TownOfUs.Options.Modifiers.Alliance;
 using TownOfUs.Utilities;
+using TownOfUs.Utilities.Appearances;
 using UnityEngine;
 
 namespace TownOfExtra.Buttons;
 
-public sealed class BloodlustKillButton : TownOfUsKillRoleButton<BloodlustRole, PlayerControl>, IDiseaseableButton, IKillButton
+public sealed class BloodlustKillButton : TownOfUsKillRoleButton<BloodlustRole, PlayerControl>, IKillButton, IDiseaseableButton
 {
     public override string Name => "Kill";
     public override BaseKeybind Keybind => Keybinds.PrimaryAction;
     public override Color TextOutlineColor => Palette.ImpostorRed;
-    public override float Cooldown => GameOptionsManager.Instance.currentNormalGameOptions.KillCooldown - OptionGroupSingleton<BloodlustRoleOptions>.Instance.LowerKillCooldown;
+    public override float Cooldown => game.Killcooldown - OptionGroupSingleton<BloodlustOptions>.Instance.ReducedKillCooldown
     public override LoadableAsset<Sprite> Sprite => TouAssets.KillSprite;
 
     public void SetDiseasedTimer(float multiplier)
@@ -26,20 +29,32 @@ public sealed class BloodlustKillButton : TownOfUsKillRoleButton<BloodlustRole, 
         SetTimer(Cooldown * multiplier);
     }
 
+    public override PlayerControl GetTarget()
+    {
+        var genOpt = OptionGroupSingleton<GeneralOptions>.Instance;
+        var saboOpt = OptionGroupSingleton<AdvancedSabotageOptions>.Instance;
+        var closePlayer = PlayerControl.LocalPlayer.GetClosestLivingPlayer(true, Distance);
+
+        var includePostors = genOpt.FFAImpostorMode ||
+                             (PlayerControl.LocalPlayer.IsLover() &&
+                              OptionGroupSingleton<LoversOptions>.Instance.LoverKillTeammates) ||
+                             (saboOpt.KillDuringCamoComms &&
+                              closePlayer?.GetAppearanceType() == TownOfUsAppearances.Camouflage);
+        if (!OptionGroupSingleton<LoversOptions>.Instance.LoversKillEachOther && PlayerControl.LocalPlayer.IsLover())
+        {
+            return PlayerControl.LocalPlayer.GetClosestLivingPlayer(includePostors, Distance, false,
+                x => !x.IsLover());
+        }
+
+        return PlayerControl.LocalPlayer.GetClosestLivingPlayer(includePostors, Distance);
+    }
+
     protected override void OnClick()
     {
         if (Target == null) return;
 
-        PlayerControl.LocalPlayer.RpcSpecialMurder(Target, MeetingCheck.OutsideMeeting, causeOfDeath: "Massacred");
-    }
-
-    public override PlayerControl GetTarget()
-    {
-        if (!OptionGroupSingleton<LoversOptions>.Instance.LoversKillEachOther && PlayerControl.LocalPlayer.IsLover())
-        {
-            return PlayerControl.LocalPlayer.GetClosestLivingPlayer(true, Distance, false, x => !x.IsLover());
-        }
-
-        return PlayerControl.LocalPlayer.GetClosestLivingPlayer(true, Distance);
+        PlayerControl.LocalPlayer.RpcSpecialMurder(
+            Target, MeetingCheck.OutsideMeeting
+        );
     }
 }
